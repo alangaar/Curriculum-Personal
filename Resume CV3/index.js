cf6d1d015c0fc6ec91dd0163fd786b4aa2ec421e
alangaar <alangaar@gmail.com>
2022-03-01 21:33:05 -0400

    Actualizacion CV

--- Resume CV3/index.js
+++ Resume CV3/index.js
@@ -11,14 +11,7 @@ app.get('/', function(request, response) {
 response.sendFile(path.join(__dirname, '/vistas/index.html'));
 }) 
 
-/*app.get('/tarjeta', function(request, response) { 
-    response.sendFile(path.join(__dirname, '/vista/tarjeta.html'));
-})*/
 
-/*app.get('/contacto', function(request, response) { 
-    response.sendFile(path.join(__dirname, '/vista/contacto.html'));
-})
-*/
 // asignacion el puerto en el que correra la app
 app.listen(3000, function() {
     console.log('Servidor escuchando en el puerto 3000');
